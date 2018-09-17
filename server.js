29e213b856ad09d82468bc6a3e20ea3cc05ad182
Edgar Segura <edsegura30>
2018-09-16 20:43:37 -0500

    Adds required URL

--- server.js
+++ server.js
@@ -16,5 +16,13 @@ app.get('/hello', (req, res) => {
   res.send({hello: "world"});
 });
 
+app.post('/hello', (req, res) => {
+  res.send("Sending post with username");
+});
+
+app.post('/auth', (req, res) => {
+  res.send("Hrere will go the user Auth");
+})
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
