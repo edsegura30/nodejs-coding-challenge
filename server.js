cfad2d28a0e8950cf0377f84f61865dfac0e3771
Edgar Segura <edsegura30>
2018-09-16 21:48:59 -0500

    POST now saves users

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 const express = require('express');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var User = require('./user/User')
+var mongoose = require('mongoose');
 
 // Constants
 const PORT = 3000;
@@ -21,12 +23,48 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 // URLs
+app.get('/', (req, res) => {
+  res.send("Index");
+});
+
 app.get('/hello', (req, res) => {
-  res.send({hello: "world"});
+  res.send({
+    hello: "world"
+  });
 });
 
 app.post('/hello', (req, res) => {
-  res.send(req.body);
+  var username = req.body.user;
+  var password = req.body.password;
+  var confPassword = req.body.confPassword;
+  console.log(User);
+  if (password !== confPassword) {
+    res.status(400);
+    res.send({
+      error: "Passwords must match"
+    });
+  } else if(
+    username.length === 0 || password.length === 0){
+      res.status(400);
+      res.send({
+        error: "Incomplete info"
+      });
+  } else if(password.length < 8){
+    res.status(400);
+    res.send({
+      error: "Password must be 8 characters long at least"
+    });
+  }
+  else {
+    res.status(201);
+    User.create({
+      user: username,
+      password: confPassword
+    });
+    res.send({
+      created: true
+    });
+  }
 });
 
 app.post('/auth', (req, res) => {
