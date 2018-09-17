4a961f5c9014f3767ab056cf30513bad69ffe9bd
Edgar Segura <edsegura30>
2018-09-16 21:06:56 -0500

    Prints data from POST request

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 // Dependencies
 const express = require('express');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 // Constants
 const PORT = 3000;
@@ -11,13 +13,20 @@ const HOST = '0.0.0.0';
 
 const app = express()
 
+// Configure expressInstance
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
 // URLs
 app.get('/hello', (req, res) => {
   res.send({hello: "world"});
 });
 
 app.post('/hello', (req, res) => {
-  res.send("Sending post with username");
+  res.send(req.body);
 });
 
 app.post('/auth', (req, res) => {
